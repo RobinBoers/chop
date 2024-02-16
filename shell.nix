51de4b48102bc3fdaf053e9d1a7c6941ec56b2e2
Robin Boers <robindev2019@outlook.com>
2024-02-16 20:37:54 +0100

    Remove outdated shell.nix

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-
-let inherit (lib) optional optionals; in mkShell {
-  buildInputs = [ bun minify optipng pngcrush jpegoptim imagemagick nodePackages.terser nodePackages.svgo nodePackages.prettier ];
-}
